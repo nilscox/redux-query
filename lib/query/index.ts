cc6c1e675c13668d5579caf2a57c14adfe0231d1
nils <nils@epita.fr>
2022-08-01 18:19:36 +0200

    turn query.reducer into a function returning the reducer

--- lib/query/index.ts
+++ lib/query/index.ts
@@ -1,12 +1,14 @@
-import { Queries, QueryState } from './query-types';
-import { queryReducer } from './query-reducer';
 import { queryActions } from './query-actions';
+import { queryReducer } from './query-reducer';
 import { querySelectors } from './query-selectors';
+import { Queries, QueryState } from './query-types';
 
 export { QueryState };
 
 export const query = <K, R, E = unknown>(name: string) => ({
-  reducer: queryReducer<K, R, E>(name),
+  reducer() {
+    return queryReducer<K, R, E>(name);
+  },
 
   actions() {
     return queryActions<K, R, E>(name);
