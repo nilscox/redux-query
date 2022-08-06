c85dddc3f4698f40ab7db21017c8fdbcbd76eb8b
nils <nils@epita.fr>
2022-08-06 23:19:57 +0200

    return a type guard along with the action creator

--- lib/utils/create-action.ts
+++ lib/utils/create-action.ts
@@ -2,16 +2,25 @@ import { Action } from 'redux';
 
 type PayloadAction<T, P> = Action<T> & P;
 
-export function createAction<Type extends string>(type: Type): () => Action<Type>;
+type ActionCreator<Type> = () => Action<Type>;
+type PayloadActionCreator<Type, Args extends unknown[], Payload> = (
+  ...args: Args
+) => PayloadAction<Type, Payload>;
+
+type ActionTypeGuard<ActionType extends Action> = (action: Action) => action is ActionType;
+
+export function createAction<Type extends string>(
+  type: Type
+): [ActionCreator<Type>, ActionTypeGuard<Action<Type>>];
 
 export function createAction<Type extends string, Args extends unknown[], Payload>(
   type: Type,
   getPayload: (...args: Args) => Payload
-): (...args: Args) => PayloadAction<Type, Payload>;
+): [PayloadActionCreator<Type, Args, Payload>, ActionTypeGuard<PayloadAction<Type, Payload>>];
 
 export function createAction<Type extends string, Args extends unknown[], Payload>(
   type: Type,
   getPayload?: (...args: Args) => Payload
 ) {
-  return (...args: Args) => ({ type, ...getPayload?.(...args) });
+  return [(...args: Args) => ({ type, ...getPayload?.(...args) }), (action: Action) => action.type === type];
 }
